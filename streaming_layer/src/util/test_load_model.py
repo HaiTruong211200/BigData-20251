014ed0dcdaca8223866553b985c2c7175676ec4e
Hai <hait20490@gmail.com>
2025-12-20 18:32:22 +0700

    feat: implement k8s for ingestion job and etl job

--- streaming_layer/src/util/test_load_model.py
+++ streaming_layer/src/util/test_load_model.py
@@ -2,8 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark.ml import PipelineModel
 import os
 
-os.environ["PYSPARK_PYTHON"] = r"C:\Users\Lenovo\miniconda3\envs\pyspark_3.10\python.exe"
-os.environ["PYSPARK_DRIVER_PYTHON"] = r"C:\Users\Lenovo\miniconda3\envs\pyspark_3.10\python.exe"
+# os.environ["PYSPARK_PYTHON"] = r"C:\Users\Lenovo\miniconda3\envs\pyspark_3.10\python.exe"
+# os.environ["PYSPARK_DRIVER_PYTHON"] = r"C:\Users\Lenovo\miniconda3\envs\pyspark_3.10\python.exe"
 
 spark = SparkSession.builder \
     .appName("LoadModel") \
